95249e34a6e3c7eb1e74be00c66164565d8e5efd
dohieu1852002 <dohieu825@gmail.com>
2023-11-04 22:09:38 +0700

    bottom navigation bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,12 @@ android {
             )
         }
     }
+    buildFeatures {
+        viewBinding = true
+    }
+
+
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
--- app/src/main/java/com/example/myapplication/secondActivity.java
+++ app/src/main/java/com/example/myapplication/secondActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,12 +11,44 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-public class secondActivity extends AppCompatActivity {
+import com.example.myapplication.databinding.ActivityMainBinding;
+import com.example.myapplication.databinding.ActivitySecondBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-    //ImageView power_btn = (ImageView) findViewById(R.id.powerbtn);
+public class secondActivity extends AppCompatActivity {
+    private ActivitySecondBinding binding;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second);
+        binding = ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new homeFragment());
+        binding.bottomNavigationView.setSelectedItemId(R.id.home);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+        int id = item.getItemId();
+        if( id == R.id.location)
+        {
+            replaceFragment(new locationfragment());
+        }else if( id == R.id.home)
+        {
+            replaceFragment(new homeFragment());
+        }
+        else if( id == R.id.infor)
+        {
+            replaceFragment(new inforFragment());
+        }
+            return true;
+        });
+
     }
+    private void replaceFragment(Fragment fragment)
+    {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
+    }
+
 }
