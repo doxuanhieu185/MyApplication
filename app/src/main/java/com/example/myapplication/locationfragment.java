f9d46ec181209395a6fa9f3db6bacfeeb5d55d2b
dohieu1852002 <dohieu825@gmail.com>
2023-12-05 18:42:37 +0700

    current city and country

--- app/src/main/java/com/example/myapplication/locationfragment.java
+++ app/src/main/java/com/example/myapplication/locationfragment.java
@@ -1,64 +1,170 @@
 package com.example.myapplication;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link locationfragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.myapplication.databinding.FragmentLocationBinding;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationAvailability;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
 public class locationfragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public locationfragment() {
-        // Required empty public constructor
-    }
+    FusedLocationProviderClient client;
+    private Context context;
+    TextView country;
+    TextView city;
+    TextView latitude;
+    TextView longitude;
+    Button getLocation;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment locationfragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static locationfragment newInstance(String param1, String param2) {
-        locationfragment fragment = new locationfragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    private final static int REQUEST_CODE = 100;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_location, container, false);
+        View view = inflater.inflate(R.layout.fragment_location,container,false);
+        country = view.findViewById(R.id.country);
+        city = view.findViewById(R.id.city);
+
+
+        client = LocationServices.getFusedLocationProviderClient(getActivity());
+        //check condition
+
+//        getLocation.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // check condition
+        if(ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(getActivity(),Manifest.permission.ACCESS_COARSE_LOCATION)
+                        == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        }else{
+                    requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION
+                    ,Manifest.permission.ACCESS_COARSE_LOCATION},100);
+        }
+
+
+//            }
+//        });
+
+
+//        return inflater.inflate(R.layout.fragment_location, container, false);
+        return view;
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //check condition
+        if (requestCode == 100 && (grantResults.length > 0) &&
+                (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED))
+        {
+            getCurrentLocation();
+        }else {
+            Toast.makeText(getActivity(), "Permission denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation() {
+        //
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        // kiem tra quyen
+        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER))
+        {
+            client.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
+                @Override
+                public void onComplete(@NonNull Task<Location> task) {
+                    Location location = task.getResult();
+
+                    //check condtion
+                    if(location != null)
+                    {
+
+
+                        Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+
+                        List<Address> address = null;
+                        try {
+                            address = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                            city.setText(address.get(0).getLocality());
+                            country.setText(address.get(0).getCountryName());
+
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+
+                    }else{
+                        LocationRequest locationRequest = new LocationRequest()
+                                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                                .setInterval(10000)
+                                .setFastestInterval(1000)
+                                .setNumUpdates(1);
+
+                        //
+
+                        LocationCallback locationCallback = new LocationCallback() {
+                            @Override
+                            public void onLocationResult(@NonNull LocationResult locationResult) {
+                                Location location1 = locationResult.getLastLocation();
+                                //set latitude
+                                latitude.setText(String.valueOf(location1.getLatitude()));
+
+                                longitude.setText(String.valueOf(location1.getLongitude()));
+                            }
+                        };
+
+                        //request location update
+
+                        client.requestLocationUpdates(locationRequest,locationCallback, Looper.myLooper());
+                    }
+                }
+            });
+        }else
+        {
+            startActivity( new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    .setFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
+    }
+
 }
